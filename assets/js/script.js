8132e09553c687295c1f296e9e7acb6e8a3c57c2
adishawarma <aditya10102004@gmail.com>
2026-05-20 13:26:14 +0530

    made some text changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["web development", "cyber security"],
+    strings: ["web development", "AI/ML"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
